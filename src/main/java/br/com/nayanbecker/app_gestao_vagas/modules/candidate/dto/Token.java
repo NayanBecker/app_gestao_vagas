eb2a4f91cf13d777cdfc92eaf34ad4c64ad497d2
nayanbecker <nayanbecker@gmail.com>
2025-02-11 13:22:14 -0300

    feat: implement security configuration and enhance candidate authentication flow

--- src/main/java/br/com/nayanbecker/app_gestao_vagas/modules/candidate/dto/Token.java
+++ src/main/java/br/com/nayanbecker/app_gestao_vagas/modules/candidate/dto/Token.java
@@ -1,12 +1,12 @@
 package br.com.nayanbecker.app_gestao_vagas.modules.candidate.dto;
 
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class Token {
     private String access_token;
-
-    public String getAccessToken() {
-        return access_token;
-    }
-    public void setAccessToken(String access_token) {
-        this.access_token = access_token;
-    }
+    private List<String> roles;
+    private Long expires_in;
 }
