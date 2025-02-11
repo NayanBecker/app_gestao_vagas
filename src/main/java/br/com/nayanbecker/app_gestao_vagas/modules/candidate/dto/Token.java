a156cd857e48a120fad7b5d6c3bd34250dc9367b
nayanbecker <nayanbecker@gmail.com>
2025-02-11 10:44:11 -0300

    add candidate login and profile functionality with error handling

---
+++ src/main/java/br/com/nayanbecker/app_gestao_vagas/modules/candidate/dto/Token.java
@@ -0,0 +1,12 @@
+package br.com.nayanbecker.app_gestao_vagas.modules.candidate.dto;
+
+public class Token {
+    private String access_token;
+
+    public String getAccessToken() {
+        return access_token;
+    }
+    public void setAccessToken(String access_token) {
+        this.access_token = access_token;
+    }
+}
