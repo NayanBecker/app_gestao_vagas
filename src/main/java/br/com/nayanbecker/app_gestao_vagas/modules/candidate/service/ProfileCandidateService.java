ebcc539f9deb9d17f0be4a3795f018eadc02fc39
nayanbecker <nayanbecker@gmail.com>
2025-02-14 11:36:46 -0300

    feat: add error handling for unauthorized access in ProfileCandidateService and update profile navigation links

--- src/main/java/br/com/nayanbecker/app_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
+++ src/main/java/br/com/nayanbecker/app_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
@@ -5,23 +5,29 @@ import java.util.Map;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.nayanbecker.app_gestao_vagas.modules.candidate.dto.ProfileUserDto;
 
 @Service
 public class ProfileCandidateService {
-    
-    public ProfileUserDto execute(String token){
+
+    public ProfileUserDto execute(String token) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
 
-        var result = restTemplate.exchange("http://localhost:8080/candidate/", HttpMethod.GET, request, ProfileUserDto.class);
-        System.out.println(result);
-        return result.getBody();
+        try {
+            var result = restTemplate.exchange("http://localhost:8080/candidate/", HttpMethod.GET, request, ProfileUserDto.class);
+            System.out.println(result);
+            return result.getBody();
+        } catch (Exception e) {
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);       
+        }
     }
 }
